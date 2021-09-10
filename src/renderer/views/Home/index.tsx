87ba482a5a16e4de4b675e2c2d50260ff5d5d2ae
hamlin.li <sclihuiming@163.com>
2021-09-10 16:58:02 +0800

    adjust sidebar

--- src/renderer/views/Home/index.tsx
+++ src/renderer/views/Home/index.tsx
@@ -39,9 +39,11 @@ function Home(props: any) {
   const pathname = get(props, 'location.pathname', '');
   const [collapsed, onCollapse] = useState(false);
   const [selectedKey, setSelectedKey] = useState(pathname);
+  const [headerSelectedKey, setHeaderSelectedKey] = useState();
   const [openKey, setOpenKey] = useState([] as string[]);
 
   useEffect(() => {}, []);
+
   useEffect(() => {
     const item = find(routerTree, (info) => pathname === info.routerPath);
     const parentPath: string = get(item, 'parentPath', '');
@@ -59,65 +61,68 @@ function Home(props: any) {
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+      <Header className="header">
         <div className={`logo ${collapsed ? 'collapsed' : ''}`} />
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={[selectedKey]}
-          selectedKeys={[selectedKey]}
-          openKeys={openKey}
-          mode="inline"
-          // onSelect={onSelect}
-          onOpenChange={onOpenChange}
-        >
-          <Menu.Item key="/home" icon={<PieChartOutlined />}>
-            <Link to={`${match.url}`}>概览</Link>
-          </Menu.Item>
-          <SubMenu
-            key="accountManager"
-            icon={<UserOutlined />}
-            title="账户管理"
-          >
-            <Menu.Item key="/home/manager-accounts">
-              <Link to={`${match.url}/manager-accounts`}>管理账户</Link>
-            </Menu.Item>
-            <Menu.Item key="/home/add-accounts">
-              <Link to={`${match.url}/add-accounts`}>增加账户</Link>
-            </Menu.Item>
-            <Menu.Item key="/home/add-ledger-accounts">
-              <Link to={`${match.url}/add-ledger-accounts`}>
-                ledger添加账户
-              </Link>
-            </Menu.Item>
-          </SubMenu>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">功能</Menu.Item>
+          <Menu.Item key="2">DApp</Menu.Item>
+          <Menu.Item key="3">关于</Menu.Item>
         </Menu>
-      </Sider>
+      </Header>
+
       <Layout className="site-layout">
-        <Header className="header">
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">功能</Menu.Item>
-            <Menu.Item key="2">DApp</Menu.Item>
-            <Menu.Item key="3">关于</Menu.Item>
-          </Menu>
-        </Header>
-        <Content style={{ margin: '10px' }}>
-          <div
-            className="site-layout-background"
-            style={{ padding: 24, minHeight: 360 }}
+        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+          <Menu
+            theme="dark"
+            defaultSelectedKeys={[selectedKey]}
+            selectedKeys={[selectedKey]}
+            openKeys={openKey}
+            mode="inline"
+            // onSelect={onSelect}
+            onOpenChange={onOpenChange}
           >
-            <Switch>
-              <Route path={`${match.path}/overview`} component={Overview} />
-              <Route
-                path={`${match.path}/add-accounts`}
-                component={AddAccount}
-              />
-              <Route path={`${match.path}`} component={Overview} />
-            </Switch>
-          </div>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          TronLink Desktop ©2021 sclihuiming@163.com
-        </Footer>
+            <Menu.Item key="/home" icon={<PieChartOutlined />}>
+              <Link to={`${match.url}`}>概览</Link>
+            </Menu.Item>
+            <SubMenu
+              key="accountManager"
+              icon={<UserOutlined />}
+              title="账户管理"
+            >
+              <Menu.Item key="/home/manager-accounts">
+                <Link to={`${match.url}/manager-accounts`}>管理账户</Link>
+              </Menu.Item>
+              <Menu.Item key="/home/add-accounts">
+                <Link to={`${match.url}/add-accounts`}>增加账户</Link>
+              </Menu.Item>
+              <Menu.Item key="/home/add-ledger-accounts">
+                <Link to={`${match.url}/add-ledger-accounts`}>
+                  ledger添加账户
+                </Link>
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: '10px' }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
+              <Switch>
+                <Route path={`${match.path}/overview`} component={Overview} />
+                <Route
+                  path={`${match.path}/add-accounts`}
+                  component={AddAccount}
+                />
+                <Route path={`${match.path}`} component={Overview} />
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            TronLink Desktop ©2021 sclihuiming@163.com
+          </Footer>
+        </Layout>
       </Layout>
     </Layout>
   );
