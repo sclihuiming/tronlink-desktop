c7bcb5c77d8ed2658167f517caacf24184f06f4e
hamlin.li <sclihuiming@163.com>
2021-09-15 12:25:11 +0800

    Important: update main.ts

---
+++ src/main/controller/windowController.ts
@@ -0,0 +1,109 @@
+/* eslint global-require: off, no-console: off */
+import { app, BrowserWindow, shell } from 'electron';
+import path from 'path';
+import log from 'electron-log';
+import { autoUpdater } from 'electron-updater';
+import { addWindow, deleteWindow } from '../store/windowManager';
+import MenuBuilder from '../menu';
+import { resolveHtmlPath } from '../../utils';
+import * as mainTool from '../mainTool';
+
+let mainWindow: BrowserWindow | null = null;
+
+const installExtensions = async () => {
+  const installer = require('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ['REACT_DEVELOPER_TOOLS'];
+
+  return installer
+    .default(
+      extensions.map((name) => installer[name]),
+      forceDownload
+    )
+    .catch(console.log);
+};
+
+export default class AppUpdater {
+  constructor() {
+    log.transports.file.level = 'info';
+    autoUpdater.logger = log;
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+}
+
+export async function createMainWindow() {
+  if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.DEBUG_PROD === 'true'
+  ) {
+    await installExtensions();
+  }
+
+  const RESOURCES_PATH = app.isPackaged
+    ? path.join(process.resourcesPath, 'assets')
+    : path.join(__dirname, '../../assets');
+
+  const getAssetPath = (...paths: string[]): string => {
+    return path.join(RESOURCES_PATH, ...paths);
+  };
+
+  mainWindow = new BrowserWindow({
+    show: false,
+    width: 1224,
+    height: 728,
+    icon: getAssetPath('icon.png'),
+    webPreferences: {
+      preload: path.join(__dirname, '../../preload/preload.common.js'),
+      contextIsolation: false,
+      nodeIntegration: true,
+      webviewTag: true,
+    },
+  });
+
+  // console.log('resolveHtmlPath:', resolveHtmlPath('index.html'));
+  // mainWindow.loadURL(resolveHtmlPath('index.html'));
+  mainWindow.loadURL('http://123.56.166.152:18096/#/home');
+
+  // @TODO: Use 'ready-to-show' event
+  //        https://github.com/electron/electron/blob/main/docs/api/browser-window.md#using-ready-to-show-event
+  mainWindow.webContents.on('did-finish-load', () => {
+    if (!mainWindow) {
+      throw new Error('"mainWindow" is not defined');
+    }
+    if (process.env.START_MINIMIZED) {
+      mainWindow.minimize();
+    } else {
+      mainWindow.show();
+      mainWindow.focus();
+    }
+  });
+
+  // load init Data
+  mainTool.run();
+
+  addWindow(mainWindow);
+
+  mainWindow.on('closed', () => {
+    deleteWindow(<BrowserWindow>mainWindow);
+    mainWindow = null;
+  });
+
+  const menuBuilder = new MenuBuilder(mainWindow);
+  menuBuilder.buildMenu();
+
+  // Open urls in the user's browser
+  mainWindow.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    shell.openExternal(url);
+  });
+
+  // Remove this if your app does not use auto updates
+  // eslint-disable-next-line
+  new AppUpdater();
+}
+
+export function activateWindow() {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createMainWindow();
+}
--- src/main/main.ts
+++ src/main/main.ts
@@ -10,27 +10,11 @@
  */
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import path from 'path';
-import { app, BrowserWindow, shell } from 'electron';
-import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
-import MenuBuilder from './menu';
-import { resolveHtmlPath } from '../utils';
-import { getDBInstance } from './store';
-import * as mainTool from './mainTool';
-import { addWindow, deleteWindow } from './store/windowManager';
-
-export default class AppUpdater {
-  constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
-  }
-}
-
-let mainWindow: BrowserWindow | null = null;
-
-mainTool.run();
+import { app } from 'electron';
+import {
+  createMainWindow,
+  activateWindow,
+} from './controller/windowController';
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -44,90 +28,6 @@ if (isDevelopment) {
   require('electron-debug')();
 }
 
-const installExtensions = async () => {
-  const installer = require('electron-devtools-installer');
-  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-  const extensions = ['REACT_DEVELOPER_TOOLS'];
-
-  return installer
-    .default(
-      extensions.map((name) => installer[name]),
-      forceDownload
-    )
-    .catch(console.log);
-};
-
-const createWindow = async () => {
-  if (
-    process.env.NODE_ENV === 'development' ||
-    process.env.DEBUG_PROD === 'true'
-  ) {
-    await installExtensions();
-  }
-
-  const RESOURCES_PATH = app.isPackaged
-    ? path.join(process.resourcesPath, 'assets')
-    : path.join(__dirname, '../../assets');
-
-  const getAssetPath = (...paths: string[]): string => {
-    return path.join(RESOURCES_PATH, ...paths);
-  };
-
-  mainWindow = new BrowserWindow({
-    show: false,
-    width: 1224,
-    height: 728,
-    icon: getAssetPath('icon.png'),
-    webPreferences: {
-      preload: path.join(__dirname, '../preload/preload.common.js'),
-      contextIsolation: false,
-      nodeIntegration: true,
-      webviewTag: true,
-    },
-  });
-
-  // console.log('resolveHtmlPath:', resolveHtmlPath('index.html'));
-  // mainWindow.loadURL(resolveHtmlPath('index.html'));
-  mainWindow.loadURL('http://123.56.166.152:18096/#/home');
-
-  // load init Data
-  const db = await getDBInstance();
-
-  // @TODO: Use 'ready-to-show' event
-  //        https://github.com/electron/electron/blob/main/docs/api/browser-window.md#using-ready-to-show-event
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    if (process.env.START_MINIMIZED) {
-      mainWindow.minimize();
-    } else {
-      mainWindow.show();
-      mainWindow.focus();
-    }
-  });
-
-  addWindow(mainWindow);
-
-  mainWindow.on('closed', () => {
-    deleteWindow(<BrowserWindow>mainWindow);
-    mainWindow = null;
-  });
-
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
-
-  // Open urls in the user's browser
-  mainWindow.webContents.on('new-window', (event, url) => {
-    event.preventDefault();
-    shell.openExternal(url);
-  });
-
-  // Remove this if your app does not use auto updates
-  // eslint-disable-next-line
-  new AppUpdater();
-};
-
 /**
  * Add event listeners...
  */
@@ -140,10 +40,6 @@ app.on('window-all-closed', () => {
   }
 });
 
-app.whenReady().then(createWindow).catch(console.log);
+app.whenReady().then(createMainWindow).catch(console.log);
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow();
-});
+app.on('activate', activateWindow);
